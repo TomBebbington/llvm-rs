0f5196e8eca4128d5b6a3328646fa36bbb97f8da
Tom Bebbington <tophattedcoder@gmail.com>
2015-06-18 11:48:01 +0100

    Improve documentation

--- src/value.rs
+++ src/value.rs
@@ -9,15 +9,15 @@ use context::{Context, GetContext};
 use ty::{FunctionType, Type};
 use util;
 
-/// A typed value that can be used as an operand in instructions
+/// A typed value that can be used as an operand in instructions.
 pub struct Value;
 native_ref!(&Value = LLVMValueRef);
 impl Value {
-    /// Create a new constant struct from the values given
+    /// Create a new constant struct from the values given.
     pub fn new_struct<'a>(context: &'a Context, vals: &[&'a Value], packed: bool) -> &'a Value {
         unsafe { core::LLVMConstStructInContext(context.into(), vals.as_ptr() as *mut LLVMValueRef, vals.len() as c_uint, packed as c_int) }.into()
     }
-    /// Create a new constant C string from the text given
+    /// Create a new constant C string from the text given.
     pub fn new_string<'a>(context: &'a Context, text: &str, rust_style: bool) -> &'a Value {
         unsafe {
             let ptr = text.as_ptr() as *const c_char;
@@ -25,23 +25,26 @@ impl Value {
             core::LLVMConstStringInContext(context.into(), ptr, len, rust_style as c_int).into()
         }
     }
+    /// Returns the name of this value, or `None` if itlacks a name
     pub fn get_name(&self) -> Option<&str> {
         unsafe {
             let c_name = core::LLVMGetValueName(self.into());
             util::to_null_str(c_name as *mut i8)
         }
     }
+    /// Sets the name of this value
     pub fn set_name(&self, name: &str) {
         let c_name = CString::new(name).unwrap();
         unsafe {
             core::LLVMSetValueName(self.into(), c_name.as_ptr())
         }
     }
+    /// Returns the type of this value
     pub fn get_type(&self) -> &Type {
         unsafe { core::LLVMTypeOf(self.into()) }.into()
     }
 }
-/// A value that represents an argument that has been given to a function
+/// A value that represents an argument that has been given to a function.
 pub struct Arg;
 native_ref!(&Arg = LLVMValueRef);
 impl Deref for Arg {
@@ -110,32 +113,35 @@ impl Index<usize> for Function {
     }
 }
 impl Function {
+    /// Add a basic block with the name given to the function and return it.
     pub fn append<'a>(&'a self, name: &str) -> &'a BasicBlock {
         util::with_cstr(name, |ptr| unsafe {
             core::LLVMAppendBasicBlockInContext(self.get_context().into(), self.into(), ptr).into()
         })
     }
+    /// Returns the entry block of this function or `None` if there is none.
     pub fn get_entry(&self) -> Option<&BasicBlock> {
         unsafe { mem::transmute(core::LLVMGetEntryBasicBlock(self.into())) }
     }
-
+    /// Returns the name of this function.
     pub fn get_name(&self) -> &str {
         unsafe {
             let c_name = core::LLVMGetValueName(self.into());
             util::to_str(c_name as *mut i8)
         }
     }
+    /// Returns the function signature representing this function's signature.
     pub fn get_signature(&self) -> &FunctionType {
         unsafe {
             let ty = core::LLVMTypeOf(self.into());
             core::LLVMGetElementType(ty).into()
         }
     }
-    /// Add an attribute to this function
+    /// Add an attribute to this function.
     pub fn add_attribute(&self, attr: Attribute) {
         unsafe { core::LLVMAddFunctionAttr(self.into(), attr.into()) }
     }
-    /// Add attributes to this function
+    /// Add attributes to this function.
     pub fn add_attributes(&self, attrs: &[Attribute]) {
         let mut sum = LLVMAttribute::empty();
         for attr in attrs {
@@ -144,14 +150,14 @@ impl Function {
         }
         unsafe { core::LLVMAddFunctionAttr(self.into(), sum.into()) }
     }
-    /// Check if the attribute is set
+    /// Check if the attribute is set.
     pub fn has_attribute(&self, attr: Attribute) -> bool {
         unsafe {
             let other = core::LLVMGetFunctionAttr(self.into());
             other.contains(attr.into())
         }
     }
-    /// Check if this function has all the attributes given
+    /// Check if this function has all the attributes given.
     pub fn has_attributes(&self, attrs: &[Attribute]) -> bool {
         unsafe {
             let other = core::LLVMGetFunctionAttr(self.into());
@@ -163,7 +169,7 @@ impl Function {
             return true;
         }
     }
-    /// Remove an attribute from the function
+    /// Remove an attribute from the function.
     pub fn remove_attribute(&self, attr: Attribute) {
         unsafe { core::LLVMRemoveFunctionAttr(self.into(), attr.into()) }
     }
@@ -173,60 +179,61 @@ impl GetContext for Function {
         self.get_type().get_context()
     }
 }
+/// A way of indicating to LLVM how you want arguments / functions to be handled.
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 #[repr(C)]
 pub enum Attribute {
-    /// Zero-extended before or after call
+    /// Zero-extended before or after call.
     ZExt =              0b1,
-    /// Sign-extended before or after call
+    /// Sign-extended before or after call.
     SExt =              0b10,
-    /// Mark the function as not returning
+    /// Mark the function as not returning.
     NoReturn =          0b100,
-    /// Force argument to be passed in register
+    /// Force argument to be passed in register.
     InReg =             0b1000,
-    /// Hidden pointer to structure to return
+    /// Hidden pointer to structure to return.
     StructRet =         0b10000,
-    /// Function doesn't unwind stack
+    /// Function doesn't unwind stack.
     NoUnwind =          0b100000,
-    /// Consider to not alias after call
+    /// Consider to not alias after call.
     NoAlias =           0b1000000,
-    /// Pass structure by value
+    /// Pass structure by value.
     ByVal =             0b10000000,
-    /// Nested function static chain
+    /// Nested function static chain.
     Nest =              0b100000000,
-    /// Function doesn't access memory
+    /// Function doesn't access memory.
     ReadNone =          0b1000000000,
-    /// Function only reads from memory
+    /// Function only reads from memory.
     ReadOnly =          0b10000000000,
-    /// Never inline this function
+    /// Never inline this function.
     NoInline =          0b100000000000,
-    /// Always inline this function
+    /// Always inline this function.
     AlwaysInline =      0b1000000000000,
-    /// Optimize this function for size
+    /// Optimize this function for size.
     OptimizeForSize =   0b10000000000000,
-    /// Stack protection
+    /// Stack protection.
     StackProtect =      0b100000000000000,
-    /// Stack protection required
+    /// Stack protection required.
     StackProtectReq =   0b1000000000000000,
-    /// Alignment of parameter (5 bits) stored as log2 of alignment with +1 bias 0 means unaligned (different from align(1))
+    /// Alignment of parameter (5 bits) stored as log2 of alignment with +1 bias 0 means unaligned (different from align(1)).
     Alignment =         0b10000000000000000,
-    /// Function creates no aliases of pointer
+    /// Function creates no aliases of pointer.
     NoCapture =         0b100000000000000000,
-    /// Disable redzone
+    /// Disable redzone.
     NoRedZone =         0b1000000000000000000,
-    /// Disable implicit float instructions
+    /// Disable implicit float instructions.
     NoImplicitFloat =   0b10000000000000000000,
-    /// Naked function
+    /// Naked function.
     Naked =             0b100000000000000000000,
-    /// The source language has marked this function as inline
+    /// The source language has marked this function as inline.
     InlineHint =        0b1000000000000000000000,
-    /// Alignment of stack for function (3 bits) stored as log2 of alignment with +1 bias 0 means unaligned (different from alignstack=(1))
+    /// Alignment of stack for function (3 bits) stored as log2 of alignment with +1 bias 0 means unaligned (different from alignstack=(1)).
     StackAlignment =    0b11100000000000000000000000000,
-    /// This function returns twice
+    /// This function returns twice.
     ReturnsTwice =      0b100000000000000000000000000000,
-    /// Function must be in unwind table
+    /// Function must be in unwind table.
     UWTable =           0b1000000000000000000000000000000,
-    /// Function is called early/often, so lazy binding isn't effective
+    /// Function is called early/often, so lazy binding isn't effective.
     NonLazyBind =       0b10000000000000000000000000000000
 }
 impl From<LLVMAttribute> for Attribute {
